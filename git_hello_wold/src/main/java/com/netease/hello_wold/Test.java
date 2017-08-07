b75b2d9e765edfd5fe6435f2067b60f1f0e6a098
mengfanming <mengfanming@corp.netease.com>
2017-08-07 19:40:01 +0800

    mod

---
+++ git_hello_wold/src/main/java/com/netease/hello_wold/Test.java
@@ -0,0 +1,12 @@
+package main.java.com.netease.hello_wold;
+
+/**
+ * Created by mengfanming on 2017/8/7.
+ */
+public class Test {
+
+    public static void main(String [] args){
+        System.out.println("test ");
+    }
+
+}
